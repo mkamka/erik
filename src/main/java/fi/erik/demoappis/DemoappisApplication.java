07ea038514ebd8cbc8a4831dcddfea0140f21ef0
mkamka <miikka.kajanne@wunderdog.fi>
2022-11-01 18:37:46 +0200

    first commit

---
+++ src/main/java/fi/erik/demoappis/DemoappisApplication.java
@@ -0,0 +1,13 @@
+package fi.erik.demoappis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoappisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoappisApplication.class, args);
+	}
+
+}
